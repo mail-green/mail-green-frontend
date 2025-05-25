5ac74bf482b9aae6b31c8e158251ca4945fa43c4
jpark0506 <peterp1@inha.edu>
2025-05-25 20:08:07 +0900

    [MG-13] feat : 기본 세팅 추가

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        main: "#8FD694",
+        secondary: "#A7CBFF",
+        alert: "#EF4444",
+        disabled: "#ADAEBC",
+        text: "#6B7280",
+      },
+    },
+  },
+};
