db6bbd45f1bd61be8ea3f936d24d78afbfa852a8
jpark0506 <peterp1@inha.edu>
2025-06-06 20:14:01 +0900

    feat : PWA 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,19 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: { enabled: true },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 50000000,
+      },
+      injectRegister: "auto",
+      injectManifest: {
+        swDest: "dist/sw.js",
+      },
+    }),
+  ],
 });
